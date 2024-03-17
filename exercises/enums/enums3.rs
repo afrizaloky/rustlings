4d5343422d03024b777a5f11ed6247fb2aa886e1
Afrizal <afrizaloky@outlook.com>
2024-03-17 12:51:34 +0700

    learning

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -8,6 +8,10 @@
 // I AM NOT DONE
 
 enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
     // TODO: implement the message variant types based on their usage below
 }
 

b6ea1dbb93ce62fca906a33fb4573dacb8fbd7ca
Afrizal <afrizaloky@outlook.com>
2024-05-07 12:16:19 +0700

    learning 3

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -9,9 +9,7 @@
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-use std::num::ParseIntError;
+use std::{error, num::ParseIntError};
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
 #[derive(PartialEq, Debug)]
@@ -24,14 +22,21 @@ impl ParsePosNonzeroError {
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
-    // TODO: add another error conversion function here.
-    // fn from_parseint...
+
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    } // TODO: add another error conversion function here.
+      // fn from_parseint...
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    let outcome: Result<i64, _> = s.parse();
+    if let Err(err) = outcome {
+        return Err(ParsePosNonzeroError::from_parseint(err));
+    }
+    let x: i64 = outcome.unwrap();
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,8 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -32,10 +30,22 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
+            match &command {
+                Command::Append(size) => {
+                    let mut appended_str = string.clone();
+                    let bar_str = "bar".to_string();
+                    let bar_str = bar_str.repeat(*size);
+
+                    appended_str.push_str(&bar_str);
+                    output.push(appended_str.to_string())
+                }
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Uppercase => output.push(string.to_uppercase()),
+            }
             // TODO: Complete the function body. You can do it!
         }
         output
@@ -45,8 +55,8 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
